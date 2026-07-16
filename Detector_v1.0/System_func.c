4a2afe90e5af855260e584c2fc5307805273eb2a
Karan Diwan <karandiwan46@gmail.com>
2026-07-16 13:36:26 +0530

    System Activation Function Simulator

--- Detector_v1.0/System_func.c
+++ Detector_v1.0/System_func.c
@@ -1,45 +1,29 @@
+// System Mathematical Behaviour Simulator
+
 #include <stdio.h>
 
 int func(int inputs[], int size, int threshold) {
-    if (threshold < 0 || threshold > size) {
-        printf("Error: Threshold must be between 0 and %d\n", size);
-        return -1;
-    }
-
     int sum_val = 0;
     for (int i = 0; i < size; i++) {
-        if (inputs[i] != 0 && inputs[i] != 1) {
-            printf("Error: Input[%d] must be 0 or 1\n", i);
-            return -1;
-        }
         sum_val += inputs[i];
     }
-
     printf("Sum => %d\n", sum_val);
     printf("Threshold => %d\n", threshold);
 
-    return (sum_val > threshold) ? 1 : 0;
+    if (sum_val > threshold)
+        return 1;
+    else
+        return 0;
 }
 
 int main() {
-    int size = 16;
-    int inputs[16];
-    int threshold;
+    printf("System Activation Function: R(M, θ) = 1 if M > θ, else 0\n");
 
-    printf("System Activation Function: R(M, T) = 1 if M > T, else 0\n");
+    // Input Vector
+    int inputs[16] = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
 
-    printf("Enter threshold (0-16): ");
-    scanf("%d", &threshold);
-
-    printf("Enter 16 binary values (0 or 1):\n");
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &inputs[i]);
-    }
-
-    int output = func(inputs, size, threshold);
-    if (output != -1) {
-        printf("System Output => %d\n", output);
-    }
+    int output = func(inputs, 16, 7);
+    printf("System Output => %d\n", output);
 
     return 0;
 }
